cbc30ab06d7fd7784889df43b50f152608c7adf3
ahmad rafiansyah <arafiansyah20@gmail.com>
2026-05-19 14:55:04 +0700

    menambahkan icon web

--- promty/apps/landing/app/layout.tsx
+++ promty/apps/landing/app/layout.tsx
@@ -12,6 +12,11 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: "Promty | Automate Your Browser Work",
   description: "Automate Your Browser Work With One Sentence",
+  icons: {
+  icon: "/images/logo_promty.png",
+  shortcut: "/images/logo_promty.png",
+  apple: "/images/logo_promty.png",
+}
 };
 
 export default function RootLayout({
